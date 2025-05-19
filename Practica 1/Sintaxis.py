0140ed62e663331b5ddd62195f6b956a3c4d7e17
belenvega21 <belenvegal2109@gmail.com>
2025-05-19 16:27:00 -0600

    Comentarios
    nada

---
+++ Practica 1/Sintaxis.py
@@ -0,0 +1,22 @@
+#1-. Comentarios
+
+#comentarios de una sola linea
+
+"""
+Si tu comentario es mayor a dos lieas van dentro de 3 comillas
+"""
+#Belen practica
+
+#Miguel
+
+#2. Strings
+
+print("Hola mundo")
+print('Hola soy una linea de comando')
+
+variable1 = "Hola soy una cadena"
+print(len(variable1))
+print(variable1[2:5])
+print(variable1[2:])
+print(variable1[:4])
+
