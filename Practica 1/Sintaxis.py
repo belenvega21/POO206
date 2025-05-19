be49ebc7c655ee81ab677e18bc94163c75037506
belenvega21 <belenvegal2109@gmail.com>
2025-05-19 16:30:12 -0600

    Variables en Python
    Se agregaron variables

--- Practica 1/Sintaxis.py
+++ Practica 1/Sintaxis.py
@@ -20,3 +20,23 @@ print(variable1[2:5])
 print(variable1[2:])
 print(variable1[:4])
 
+#3. Variables
+
+#5Mike = o #no puedes crear variables con números al inicio
+
+
+x = "Belen"
+x = 5.5
+x =4
+
+print (x)
+
+x = int(3)
+y = float(3)
+z = str(3)
+
+print(x, y, z)
+
+print(type(x))
+print(type(y))
+print(type(z))
