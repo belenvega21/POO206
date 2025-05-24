85d6b484549436f4a02526b96253522cedbeab2b
belenvega21 <belenvegal2109@gmail.com>
2025-05-23 21:23:37 -0600

    Validar contraseña
    Valida de contraseña con reglas de seguridad, excepciones y repetición hasta entrada válida

---
+++ Practica 3/ValidaciónContraseña.py
@@ -0,0 +1,16 @@
+
+while True:
+        contraseña = input("Ingresa una contraseña (o escribe 'salir'): ")
+        if contraseña.lower() == "salir":
+            break
+        
+        if len(contraseña) < 10:
+            print("Contraseña demasiado corta")
+        elif not any(char.isdigit() for char in contraseña):
+            print("Debe agregar al menos un número")
+        elif not re.search(r"[!@#$%^&*(),.?\":{}|<>]", contraseña):
+            print("Debe contener al menos un carácter especial")
+        else:
+            print("Contraseña correcta")
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
