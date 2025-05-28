6a00c242bb1826e4c1394dceb32b4821de58f4e8
belenvega21 <belenvegal2109@gmail.com>
2025-05-27 23:42:59 -0600

    Cuenta apariciones de una letra
    Conteo de letras en una frase con validación y manejo de excepciones

---
+++ Practica 4/Contar apariciones de una letra.py
@@ -0,0 +1,17 @@
+# ejercicio3.py
+try:
+    frase = input("Ingresa una frase: ").strip()
+    letra = input("Ingresa una letra: ").strip()
+
+    if not frase:
+        raise ValueError("La frase no puede estar vacía.")
+    if len(letra) != 1 or not letra.isalpha():
+        raise ValueError("Debes ingresar una sola letra válida.")
+
+    conteo = frase.lower().count(letra.lower())
+    print(f"La letra '{letra}' aparece {conteo} veces en la frase.")
+
+except ValueError as ve:
+    print("Error:", ve)
+except Exception as e:
+    print("Error inesperado:", e)
