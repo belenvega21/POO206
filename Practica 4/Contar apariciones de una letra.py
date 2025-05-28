1ab1b4f7706b9a7af452b147af91ae0e24fcd51a
belenvega21 <belenvegal2109@gmail.com>
2025-05-28 15:30:04 -0600

    Update Contar apariciones de una letra.py

--- Practica 4/Contar apariciones de una letra.py
+++ Practica 4/Contar apariciones de una letra.py
@@ -1,10 +1,11 @@
-# ejercicio3.py
 try:
     frase = input("Ingresa una frase: ").strip()
     letra = input("Ingresa una letra: ").strip()
 
     if not frase:
         raise ValueError("La frase no puede estar vacía.")
+    if any(char.isdigit() for char in frase):
+        raise ValueError("La frase no debe contener números.")
     if len(letra) != 1 or not letra.isalpha():
         raise ValueError("Debes ingresar una sola letra válida.")
 
